81a8ea9afc0842201dd013ac9bd8eae52636ef85
Jorge Martínez Hurtado <45176388+jorgemhdev@users.noreply.github.com>
2021-01-15 15:02:18 +0100

    file: added Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import main.gui.Frame;
+
+/**
+ * @Author Jorge Martinez (nothing4free)
+ */
+
+public class Main {
+    public static void main(String[] args){
+        Frame gui = new Frame();
+    }
+}
